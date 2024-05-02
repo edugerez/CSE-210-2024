fdf552b192a0f028faa3647549777c074325e41a
Eduardo <113535626+edugerez@users.noreply.github.com>
2024-05-02 10:18:33 -0300

    files

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+
+        Console.WriteLine("What is your first name?");
+        string name1 = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string lastName1 = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName1}, {name1} {lastName1}.");
     }
 }
